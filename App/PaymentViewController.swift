b90a90e2e37f940cd44022bbbcc8a7268c2433e2
Tambanco <tmbnc@bk.ru>
2021-04-18 09:24:42 +0300

    added mock to paymentView

--- App/PaymentViewController.swift
+++ App/PaymentViewController.swift
@@ -6,11 +6,44 @@
 //
 
 import UIKit
-
-class PaymentViewController: UIViewController
+class PaymentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    var usersArray : Array =    [["data": "17.04.2021", "time": "09:20", "amout": "1200"],
+                                 ["data": "16.04.2021", "time": "11:40", "amout": "5600"],
+                                 ["data": "15.04.2021", "time": "02:30", "amout": "304"],
+                                 ["data": "14.04.2021", "time": "15:20", "amout": "1100"],
+                                ]
+    
+    @IBOutlet weak var tblUsers: UITableView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        let nib = UINib.init(nibName: "PaymentViewCell", bundle: nil)
+        self.tblUsers.register(nib, forCellReuseIdentifier: "PaymentViewCell")
     }
+    
+    // MARK: - UITableView delegate
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return usersArray.count
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+        {
+            return 100
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PaymentViewCell", for: indexPath) as! PaymentViewCell
+            let dict = usersArray[indexPath.row]
+            
+            cell.dataLabel.text = dict["data"]
+            cell.timeLabel.text = dict["time"]
+            cell.amountLabel.text = dict["amout"]
+            
+            return cell
+        }
 }
--- App/XIB/PaymentViewCell.swift
+++ App/XIB/PaymentViewCell.swift
@@ -14,4 +14,14 @@ class PaymentViewCell: UITableViewCell
     @IBOutlet weak var paymentLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
     
+    override func awakeFromNib()
+    {
+    super.awakeFromNib()
+    // Initialization code
+    }
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+    }
 }
