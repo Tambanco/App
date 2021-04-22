fc845e16a47fecb8aee86f3115a7f11291393dd6
Al <alexunder_5542@mail.ru>
2021-04-22 13:38:33 +0300

    added logout button configurator

--- App/Controllers/PaymentViewController.swift
+++ App/Controllers/PaymentViewController.swift
@@ -26,7 +26,6 @@ class PaymentViewController: UIViewController, UITableViewDelegate, UITableViewD
         tblUsers.rowHeight = UITableView.automaticDimension
         
         configureLogout()
-    
     }
     
     // MARK: - UITableView delegate
@@ -63,13 +62,22 @@ extension PaymentViewController
 {
     func configureLogout()
     {
-        let logoutButton = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
-         logoutButton.backgroundColor = #colorLiteral(red: 0.3333333333, green: 0.631372549, blue: 0.537254902, alpha: 1)
-        logoutButton.layer.cornerRadius = 10
-         logoutButton.setTitle("Logout", for: .normal)
-         logoutButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        let heightOfView = view.frame.height
+        let widthOfView = view.frame.width
+        let heightOfElement: CGFloat = 50.0
+        let widthOfElement: CGFloat = widthOfView - 40.0
+        let cornerRadius: CGFloat = 10
+        
+        let loginButton = UIButton(frame: CGRect(x: 20, y: heightOfView * 0.85, width: widthOfElement, height: heightOfElement))
+            loginButton.setTitle("Logout", for: .normal)
+            loginButton.backgroundColor = #colorLiteral(red: 0.3333333333, green: 0.631372549, blue: 0.537254902, alpha: 1)
+            loginButton.layer.cornerRadius = cornerRadius
+            loginButton.layer.shadowOpacity = 1
+            loginButton.layer.shadowRadius = 4.0
+            loginButton.layer.shadowColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+            loginButton.addTarget(self, action: #selector(self.buttonAction), for: .touchUpInside)
 
-         self.view.addSubview(logoutButton)
+        self.view.addSubview(loginButton)
     }
     
     @objc func buttonAction(sender: UIButton!)
